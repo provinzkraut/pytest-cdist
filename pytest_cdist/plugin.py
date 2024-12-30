f8cbc6dd6ef20043902130f1cbe45bf0531fe859
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2024-12-30 20:17:35 +0100

    some version compat

--- pytest_cdist/plugin.py
+++ pytest_cdist/plugin.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import collections
 import json
 import pathlib
--- tests/test_check.py
+++ tests/test_check.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import pytest
 from pytest_cdist.check import check
 
